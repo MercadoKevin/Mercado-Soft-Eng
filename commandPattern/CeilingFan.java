5eb1252802aed574a84e36cab3f66197d6415f41
Yohdah <Kevindalemercad@gmail.com>
2024-09-09 16:19:02 +0800

    Update CeilingFan.java

--- commandPattern/CeilingFan.java
+++ commandPattern/CeilingFan.java
@@ -2,12 +2,12 @@ package commandPattern;
 
 public class CeilingFan implements Device {
 
-    @Override
+ 
     public String switchOn() {
         return "Ceiling Fan is switched on!";
     }
 
-    @Override
+  
     public String switchOff() {
         return "Ceiling Fan is switched off!";
     }
