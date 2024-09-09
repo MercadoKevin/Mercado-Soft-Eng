b81b204f182b200ffca878d9a21ac7f4ec8bf46b
Yohdah <Kevindalemercad@gmail.com>
2024-09-09 17:21:34 +0800

    Create Stereo.java

---
+++ commandPattern/Stereo.java
@@ -0,0 +1,14 @@
+package commandPattern;
+
+public class Stereo implements Device {
+
+	 
+    public String switchOn() {
+        return "Stereo is switched on!";
+    }
+
+  
+    public String switchOff() {
+        return "Stereo is switched off!";
+    }
+}
