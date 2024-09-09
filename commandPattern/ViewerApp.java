5abe95fa40f1b4341cad663cb4e50d6f84d16ced
Yohdah <Kevindalemercad@gmail.com>
2024-09-09 17:20:52 +0800

    Update ViewerApp.java

--- commandPattern/ViewerApp.java
+++ commandPattern/ViewerApp.java
@@ -2,21 +2,27 @@ package commandPattern;
 
 public class ViewerApp {
     public static void main(String[] args){
-        Tv tv = new Tv();
-        CeilingFan cf = new CeilingFan();
+        Device tv = new Tv();
+        Device cf = new CeilingFan();
+        Device st = new Stereo();
         RemoteControl rc = new RemoteControl();
-
-        PowerOn powerOn = new PowerOn(tv);
-        System.out.println(powerOn.execute());
-
-        PowerOff powerOff = new PowerOff(tv);
-        System.out.println(powerOff.execute());
-
-        PowerOn powerOn = new PowerOn(cf);
-        System.out.println(powerOn.execute());
-
-        PowerOff powerOff = new PowerOff(cf);
-        System.out.println(powerOff.execute());
-    
+        
+        rc.setCommand(new PowerOn(cf));
+        rc.clickButton();
+       
+        rc.setCommand(new PowerOff(cf));
+        rc.clickButton();
+        
+        rc.setCommand(new PowerOn(tv));
+        rc.clickButton();
+       
+        rc.setCommand(new PowerOff(tv));
+        rc.clickButton();
+        
+        rc.setCommand(new PowerOn(st));
+        rc.clickButton();
+       
+        rc.setCommand(new PowerOff(st));
+        rc.clickButton();
     }
 }
