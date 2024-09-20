dd27192e6ce50ed6972472d7ff3e25b8c34cfba5
Yohdah <Kevindalemercad@gmail.com>
2024-09-20 22:35:41 +0800

    Update Refrigerator.java

--- AdapterPattern/Refrigerator.java
+++ AdapterPattern/Refrigerator.java
@@ -2,6 +2,6 @@ package adapterPattern;
 
 public class Refrigerator {
     public void startCooling() {
-        System.out.println("Refrigerator is cooling!");
+        System.out.println("Refrigerator is ON! now cooling!");
     }
 }
